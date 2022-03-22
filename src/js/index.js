76ca4836b7651c480814a249371146626b6ad53c
Felipe Mellão <felipe.mellao08@gmail.com>
2022-03-22 10:08:34 -0300

    Inicando JavaScript

---
+++ src/js/index.js
@@ -0,0 +1,24 @@
+/*
+Quando clicar no pokemon da listagem temos que esconder o cartão do pokemon aberto e mostrar o cartão correspondente ao que foi selecionado na listagem.
+
+Para isso vamos precisar trabalhar com dois elementos:
+1 - Listagem
+2 - Cartão do pokemon
+
+Precisamos criar duas variáveis no JS para trabalhar  com os elementos da tela.
+
+Vamos precisar trabalhar  com um evento de clique feito pelo usuário na listagem de pokemon.
+
+- Remover a classe aberto só do cartão que está aberto.
+- Ao clicar em um pokemon da listagem, pegamos o id desse pokemon para saber qual cartão mostrar.
+- Remover a classe ativo que marca o pokemon selecionado
+- Adicionar a classe ativo no item da lista selecionado
+
+
+*/
+
+
+
+// Precisamos criar duas variáveis no JS para trabalhar  com os elementos da tela.
+
+const 
